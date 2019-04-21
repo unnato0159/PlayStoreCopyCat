44c9e4316cd82a6415a363dd4225dd36b28eac6f
unnato0159 <unnato01@naver.com>
2019-04-21 11:17:19 +0900

    어댑터 클래스 생성 생성자 / 멤버변수 정리 getView 메소드 오버라이드 기초 작업

---
+++ app/src/main/java/com/tje/playstorecopycat/adapters/AppAdapter.java
@@ -0,0 +1,39 @@
+package com.tje.playstorecopycat.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import com.tje.playstorecopycat.R;
+import com.tje.playstorecopycat.datas.App;
+
+import java.util.List;
+
+public class AppAdapter extends ArrayAdapter<App> {
+
+    Context mContext;
+    List<App> mList;
+    LayoutInflater inf;
+
+    public AppAdapter(Context context, List<App> list) {
+
+        super(context, R.layout.app_list_item ,list);
+
+        mContext = context;
+        mList = list;
+        inf = LayoutInflater.from(mContext);
+    }
+
+
+    @Override
+    public View getView(int position,View convertView,ViewGroup parent) {
+        View row = convertView;
+        if(row == null){
+            row = inf.inflate(R.layout.app_list_item, null);
+        }
+
+        return row;
+    }
+}
